158e3f6f75435f8e32223d5c3c1dcac839ac4f65
Michael Levin <michael.levin@officemanagement.se>
2021-05-04 12:28:17 +0200

    Fungerande scrape

---
+++ scraper.ps1
@@ -0,0 +1,10 @@
+
+$response = Invoke-WebRequest -Uri 'https://www.matchi.se/book/schedule?wl=&facilityId=526&date=2021-05-04&sport=5&week=&year=&_=' -UseBasicParsing
+
+$content = $response.content
+
+$datePattern = 'title="Available.*'
+
+$results = $content | Select-String $datePattern -AllMatches
+
+echo $results.Matches.Value
