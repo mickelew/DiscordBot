a3e1fc79f001b441d7ebfcb0a97e5a9671af757b
Michael Levin <michael.levin@officemanagement.se>
2021-05-04 17:06:13 +0200

    Sammanslag av scraper/webhook

--- scraper.ps1
+++ scraper.ps1
@@ -1,12 +1,20 @@
-
-$response = Invoke-WebRequest -Uri 'https://www.matchi.se/book/schedule?wl=&facilityId=526&date=2021-05-04&sport=5&week=&year=&_=' -UseBasicParsing
-
-$content = $response.content
-
+#
+# Webscraper
+#
+$request = Invoke-WebRequest -Uri 'https://www.matchi.se/book/schedule?wl=&facilityId=526&date=2021-05-05&sport=5&week=&year=&_=' -UseBasicParsing
+$content = $request.content
 $datePattern = 'title="Available.*'
-
 $results = $content | Select-String $datePattern -AllMatches
+$matched = $results.Matches.Value -replace '(^title=\")|(<br>)|($*">)'
 
-$test = $results.Matches.Value -replace '(^title=\")|(<br>)|($*">)'
+#
+# Webhook
+#
+$hookUrl = "https://discord.com/api/webhooks/838669164050644992/wR2SzXBkYM7sbTqVLryFlQ96n6-1euKeW1nBorBbPJMqeUBCn0XBdj_04RWn5kXcvTu9"
+$content = $matched
 
-echo $test
+$payload = [PSCustomObject]@{ 
+    content = $content
+}
+Write-Output $content
+#Invoke-RestMethod -Uri $hookUrl -Method Post -ContentType 'Application/Json' -Body ($payload | ConvertTo-Json)
--- webhook.ps1
+++ webhook.ps1
@@ -1,7 +1,9 @@
 $hookUrl = "https://discord.com/api/webhooks/838669164050644992/wR2SzXBkYM7sbTqVLryFlQ96n6-1euKeW1nBorBbPJMqeUBCn0XBdj_04RWn5kXcvTu9"
 
 $content = @"
-Denna bot ger information om lediga padeltider.
+
+This is a padel bot.
+
 "@
 
 $payload = [PSCustomObject]@{
