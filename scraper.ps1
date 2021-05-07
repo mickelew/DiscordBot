ded7f8e0b4e1695544effaf96137db8c1753de7e
Michael Levin <michael.levin@officemanagement.se>
2021-05-07 17:07:18 +0200

    Added Index

--- scraper.ps1
+++ scraper.ps1
@@ -1,5 +1,5 @@
-# Webscraper
-$request = Invoke-WebRequest -ContentType "application/json; charset=utf-8" -Uri 'https://www.matchi.se/book/schedule?wl=&facilityId=526&date=2021-05-05&sport=5&week=&year=&_=' -UseBasicParsing
+#Webscraper
+$request = Invoke-WebRequest -ContentType "application/json; charset=utf-8" -Uri 'https://www.matchi.se/book/schedule?wl=&facilityId=526&date=2021-05-07&sport=5&week=&year=&_=' -UseBasicParsing
 $content = $request.content
 
 #Matches
@@ -11,14 +11,14 @@ $regexReplace = $results.Matches.Value -replace '(^title=\")|(<br>)|($*">)'
 $regexReplace2 = $regexReplace -replace '(?<=Available)\d+'
 
 
-# Webhook
+#Webhook
 $hookUrl = "https://discord.com/api/webhooks/838669164050644992/wR2SzXBkYM7sbTqVLryFlQ96n6-1euKeW1nBorBbPJMqeUBCn0XBdj_04RWn5kXcvTu9"
 
 #$jsonCorrected = [Text.Encoding]::UTF8.GetString([Text.Encoding]::GetEncoding(28591).GetBytes($regexReplace2))
 
-$content = $regexReplace2 | ConvertTo-Json
+$content = $regexReplace2 | Select-Object -Index 5
 $payload = [PSCustomObject]@{ 
     content = $content
 }
-Write-Output $regexReplace2
-Invoke-RestMethod -Uri $hookUrl -Method Post -ContentType 'Application/Json; charset=utf-8' -Body ($payload)
+Write-Output $regexReplace2 
+Invoke-RestMethod -Uri $hookUrl -Method Post -ContentType 'Application/Json; charset=utf-8' -Body ($payload | ConvertTo-Json)
